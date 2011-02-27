300d4ed94c16f6e1eefc23faff05e1867c64dc78
Romain Gauthier <romain.gauthier@masteri2l.org>
2011-02-27 19:47:52 +0100

    Add a setup.py file.

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Setup file for feedstail
+from setuptools import setup
+
+long_description = """\
+Feedstail monitor a feed and emits new entries. It aim to be simple,
+hackable and compatible with rsstail, its C brother."""
+
+setup( name             = "feedstail"
+     , description      = "A tail-f-like utility for feeds"
+     , long_description = long_description
+     , license          = "GNU General Public License v3"
+
+     , author           = "Romain Gauthier"
+     , author_email     = "romain.gauthier@masteri2l.org"
+
+     , version          = "0.0.1"
+     , scripts          = ['bin/feedstail']
+     , packages         = ['feedstail']
+     , data_files       = [('', ['README.rst', 'LICENSE.txt', 'feedstailrc.example'])]
+     , install_requires = ['argparse', 'FeedParser']
+
+     , classifiers      =
+         [ "Development Status :: 3 - Alpha"
+         , "License :: OSI Approved :: GNU General Public License v3"
+         , "Operating System :: OS Independent"
+         , "Programming Language :: Python :: 2.6"
+         , "Topic :: Utilities"
+         ]
+      )
+
