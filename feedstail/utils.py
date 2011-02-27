6f4ebfbda9e43af71b1e21a515d8a026d9f6713d
Romain Gauthier <romain.gauthier@masteri2l.org>
2011-02-27 19:29:17 +0100

    Utils: Add the Storage class.
    A Storage object is like a dictionary except `obj.foo` can be used in addition
    to `obj['foo']`.
    The Storage class is part of the webpy project so this class (and only this
    class) is in the public domain.

---
+++ feedstail/utils.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2011 Romain Gauthier <romain.gauthier@masteri2l.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# The following class is part of the web.py project. So it is in the Public
+# Domain. For more informations, please see the following URLs :
+#
+#   https://github.com/webpy/webpy/blob/master/LICENSE.txt
+#   https://github.com/webpy/webpy/blob/master/web/utils.py
+#
+class Storage(dict):
+    """
+    A Storage object is like a dictionary except `obj.foo` can be used
+    in addition to `obj['foo']`.
+
+        >>> o = storage(a=1)
+        >>> o.a
+        1
+        >>> o['a']
+        1
+        >>> o.a = 2
+        >>> o['a']
+        2
+        >>> del o.a
+        >>> o.a
+        Traceback (most recent call last):
+        ...
+        AttributeError: 'a'
+
+    """
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError, k:
+            raise AttributeError, k
+
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError, k:
+            raise AttributeError, k
+
+    def __repr__(self):
+        return '<Storage ' + dict.__repr__(self) + '>'
+
