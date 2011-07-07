72b3c53f7039c8206c8b4ddf13384b843a30b060
Laurent Peuch <cortex@worlddomination.be>
2011-07-07 13:17:56 +0200

    [fix] reverse entries before limiting the number of first entries

--- feedstail/feedstail.py
+++ feedstail/feedstail.py
@@ -58,13 +58,14 @@ def loop():
         global tail
         entries = parse(config.url).entries
 
+        if config.reverse:
+            entries.reverse()
+
         if number is not None and number <= len(entries):
             for entry in entries[number:]:
                 tail = [entry] + tail[:100]
             entries = entries[:number]
 
-        if config.reverse:
-            entries.reverse()
         for entry in entries:
             if isnew(entry):
                 tail = [entry] + tail[:100]
