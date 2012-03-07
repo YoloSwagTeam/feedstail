4358f4c3c3d243ddd64a58c75524c73017bf9a3a
Romain Gauthier <romain.gauthier@monkeypatch.me>
2012-03-07 21:12:58 +0100

    [mod] Bump version to 0.4.0.

--- feedstail/__init__.py
+++ feedstail/__init__.py
@@ -17,7 +17,7 @@
 from feedstail import loop, FeedKeyError
 
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __all__ = [ 'FeedKeyError'
           , '__version__'
           , 'loop'
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup( name             = "feedstail"
      , author           = "Romain Gauthier"
      , author_email     = "romain.gauthier@masteri2l.org"
 
-     , version          = '0.3.0'
+     , version          = '0.4.0'
      , scripts          = ['bin/feedstail']
      , packages         = ['feedstail']
      , data_files       = [('', ['README.rst', 'LICENSE.txt'])]
