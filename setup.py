8e2728b4f93ce3883f4c6e94079281b764dec76d
Laurent Peuch <cortex@worlddomination.be>
2014-05-23 15:20:52 +0200

    [mod] new release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup( name             = "feedstail"
      , author           = "Romain Gauthier"
      , author_email     = "romain.gauthier@masteri2l.org"
 
-     , version          = '0.5'
+     , version          = '0.5.1'
      , scripts          = ['bin/feedstail']
      , packages         = ['feedstail']
      , data_files       = [('', ['README.rst', 'LICENSE.txt'])]
